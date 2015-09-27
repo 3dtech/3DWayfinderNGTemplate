9b6f869d75c011eaf3dee289ce2dc442271b982f
salarelv <taavi.juursalu@3dtech.ee>
2015-09-28 00:03:08 +0300

    Added few gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var del = require('del');
 
 gulp.task('default', function() {
-  // place code for your default task here
+  gulp.start('clean', 'html', 'scripts');
+});
+
+gulp.task('clean', function() {
+	return del(['dist/*.*']);
+});
+
+gulp.task('scripts', function() {
+	return gulp.src('./src/modules/*.js')
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('html', function() {
+	return gulp.src('./src/index.html').pipe(gulp.dest('./dist/'));
 });
