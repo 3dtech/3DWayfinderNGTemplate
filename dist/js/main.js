98dc3f05d0e62a5ef40c78003b7e5448b103770a
salarelv <taavi.juursalu@3dtech.ee>
2015-09-28 14:53:35 +0300

    Missing stuff

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,13 @@
 // Declare app level module which depends on filters, and services
 angular.module('app', [
-  'wfangular',
-  'wf.languages'
-]);
+	'wfangular',
+	'wf.languages'
+])
+.run(['wfangular3d', function(wayfinder) {
+	WayfinderAPI.LOCATION = "http://api.3dwayfinder.com/";
+	wayfinder.options.assetsLocation = 'http://static.3dwayfinder.com/shared/';
+	wayfinder.open();
+}]);
 angular.module('wf.languages', ['wfangular'])
 .controller('LanguagesController', ['$scope', 'wfangular3d', function($scope, wayfinder) {
 		$scope.languages = {};
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // Declare app level module which depends on filters, and services
 angular.module('app', [
-  'wfangular',
-  'wf.languages'
-]);
+	'wfangular',
+	'wf.languages'
+])
+.run(['wfangular3d', function(wayfinder) {
+	WayfinderAPI.LOCATION = "http://api.3dwayfinder.com/";
+	wayfinder.options.assetsLocation = 'http://static.3dwayfinder.com/shared/';
+	wayfinder.open();
+}]);
