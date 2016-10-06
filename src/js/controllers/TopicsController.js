9de72915ab6269e90994e9d085fec34ed5a83dd4
Tõnis Terasmaa <tonis.terasmaa@3dwayfinder.com>
2016-10-06 10:55:30 +0300

    updates

--- src/js/controllers/TopicsController.js
+++ src/js/controllers/TopicsController.js
@@ -1,6 +1,6 @@
-//var groupsModule = angular.module('wf.groups', ['wfangular']);
-
-//groupsModule.controller('GroupsCtrl', [
+/**
+ * Controller to handle logic going on in topics view
+ */
 wfApp.controller('TopicsController', [
     '$rootScope',
     '$scope',
@@ -8,54 +8,61 @@ wfApp.controller('TopicsController', [
     '$routeParams',
     'wfService',
     'wfangular3d',
-    function($rootScope, $scope, $timeout, $routeParams, wfService,
-        wayfinder) {
+    function ($rootScope, $scope, $timeout, $routeParams, wfService, wayfinder) {
         console.debug("TC.loaded");
         var topics = $scope;
         $scope.groups = [];
         $scope.activeGroup = "";
         var wfTopicsDataLoaded = false;
-
         $scope.collapsedGroup = [];
         $scope.collapsedFloor = [];
         $scope.activeLetter = "";
 
-        $scope.toggleGroupActive = function(group) {
+        /**
+         * Function for expanding and collapsing a topic
+         * @param group
+         * @returns {number}
+         */
+        $scope.toggleGroupActive = function (group) {
             group.active = !group.active;
-            return 0;
         };
 
-        $scope.getLanguage = function() {
+        /**
+         * Function to fetch currently active language
+         * @returns {*}
+         */
+        $scope.getLanguage = function () {
             return wayfinder.getLanguage();
         };
 
-        $scope.expand = function(group) {
+        /**
+         * Currently unused function
+         * @param group
+         */
+        $scope.expand = function (group) {
             group.show = !group.show;
         };
 
-        $scope.$watch("collapsedGroup", function(newVal, oldVal) {
+        /**
+         * Watcher to check if a group is collapsed or not
+         */
+        $scope.$watch("collapsedGroup", function (newVal, oldVal) {
             //console.log("collapsedGroup:", oldVal, "->", newVal);
         });
 
-        /*$scope.$on("wf.data.loaded", function() {
-            $timeout(function() {
-                $scope.groups = wayfinderService.getGroups();
-                if ($routeParams) {
-                    for (var key in $scope.groups) {
-                        if ($scope.groups[key].id == $routeParams.id)
-                            $scope.groups[key].active = true;
-                        else
-                            $scope.groups[key].active = false;
-                    }
-                }
-                console.log("TopicsController-wf.data.loaded");
-                $scope.$apply();
-            }, 20);
-        });*/
 
+        /**
+         * This event is dispatched when the controller is initialized
+         * @param topics is passed along with the event, which is captured by the service
+         * which in turn will insert the data into the variable
+         */
         $rootScope.$emit("topics.init", topics);
 
-        $scope.$on('wf.topic.selected', function(event, group) {
+        /**
+         * This watcher waits for the "wf.topic.selected" event, which is sent by the AtoZ
+         * controller when the user selects a topics from the AtoZ menu
+         */
+        $scope.$on('wf.topic.selected', function (event, group) {
 
             if ($routeParams) {
                 for (var key in $scope.groups) {
@@ -64,23 +71,39 @@ wfApp.controller('TopicsController', [
             }
         });
 
-        /*topics.$on( 'wfService.groups.loading', function ( event ) {
-            console.debug( "TOPICS: wfService.loading " );
-            $timeout( function () {
-                $rootScope.$emit( "topics.init", topics );
-            }, 100 );
-        } );
+        /*$scope.$on("wf.data.loaded", function() {
+         $timeout(function() {
+         $scope.groups = wayfinderService.getGroups();
+         if ($routeParams) {
+         for (var key in $scope.groups) {
+         if ($scope.groups[key].id == $routeParams.id)
+         $scope.groups[key].active = true;
+         else
+         $scope.groups[key].active = false;
+         }
+         }
+         console.log("TopicsController-wf.data.loaded");
+         $scope.$apply();
+         }, 20);
+         });*/
 
-        topics.$on( 'wfService.groups.loaded', function ( event,
-            data ) {
-            if ( !wfTopicsDataLoaded ) {
-                $scope.$apply( function () {
-                    wfTopicsDataLoaded = true;
-                    console.debug(
-                        "TOPICS: wfService.loaded" );
-                    //topics.groups = wfService.getGroups();
-                } );
-            }
-        } );*/
+        /*topics.$on( 'wfService.groups.loading', function ( event ) {
+         console.debug( "TOPICS: wfService.loading " );
+         $timeout( function () {
+         $rootScope.$emit( "topics.init", topics );
+         }, 100 );
+         } );
+
+         topics.$on( 'wfService.groups.loaded', function ( event,
+         data ) {
+         if ( !wfTopicsDataLoaded ) {
+         $scope.$apply( function () {
+         wfTopicsDataLoaded = true;
+         console.debug(
+         "TOPICS: wfService.loaded" );
+         //topics.groups = wfService.getGroups();
+         } );
+         }
+         } );*/
     }
 ]);
