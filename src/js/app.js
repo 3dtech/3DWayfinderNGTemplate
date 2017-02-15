e5e995570991c0c9d498650773b199929999b575
Tõnis Terasmaa <tonis.terasmaa@3dwayfinder.com>
2017-02-15 13:35:11 +0200

    updated list structure and styles for tab-heads and list-POIs

--- src/js/app.js
+++ src/js/app.js
@@ -79,7 +79,7 @@ wfApp.config(['wfangularConfig', '$routeProvider', '$locationProvider', '$httpPr
 		// @ifdef type2D
 		wfConfig.mapType = "2d";
 		// @endif
-		wfConfig.apiLocation = "../../../api";
+		wfConfig.apiLocation = "//api.3dwayfinder.com/";
 		cfpLoadingBarProvider.parentSelector = '#loading-bar-container';
 		cfpLoadingBarProvider.spinnerTemplate = '<div><span class="fa fa-spinner">Custom Loading Message...</div>';
 		cfpLoadingBarProvider.latencyThreshold = 500;
