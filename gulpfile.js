ed3398fed385bfec94e9cd429e9af9bc8abd05ba
raulm <raul.muser@gmail.com>
2018-01-16 12:33:25 +0200

    Add gulp task for copying less files to dist

--- gulpfile.js
+++ gulpfile.js
@@ -21,341 +21,353 @@ var baseDir = __dirname + '/dist';
 var distFolder = "dist/";
 
 var prepOpts = {
-	context: {
-		NODE_ENV: 'development',
-		DEBUG: true,
-		type3D: true
-	}
+    context: {
+        NODE_ENV: 'development',
+        DEBUG: true,
+        type3D: true
+    }
 };
 
 var uglifyOpts = {};
 
-gulp.task('default', ['clean'], function() {
-	gulp.start('html', /*'controllers',*/ 'views', 'vendor',
-		'font', 'js', 'minifyJS',
-		'json', 'img', 'css', 'less',
-		'layout', 'rewrite');
+gulp.task('default', ['clean'], function () {
+    gulp.start('html', /*'controllers',*/ 'views', 'vendor',
+        'font', 'js', 'minifyJS',
+        'json', 'img', 'css', 'less','dist-less',
+        'layout', 'rewrite');
 });
 
-gulp.task('production-2d', ['clean'], function() {
-	uglifyOpts = {
-		compress: {
-			sequences: true,
-			properties: true,
-			dead_code: true,
-			conditionals: true,
-			comparisons: true,
-			evaluate: true,
-			booleans: true,
-			loops: true,
-			unused: true,
-			drop_console: true
-		}
-	};
-	prepOpts = {
-		context: {
-			NODE_ENV: 'production',
-			DEBUG: false,
-			type2D: true
-		}
-	};
-	gulp.start('default');
+gulp.task('production-2d', ['clean'], function () {
+    uglifyOpts = {
+        compress: {
+            sequences: true,
+            properties: true,
+            dead_code: true,
+            conditionals: true,
+            comparisons: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            drop_console: true
+        }
+    };
+    prepOpts = {
+        context: {
+            NODE_ENV: 'production',
+            DEBUG: false,
+            type2D: true
+        }
+    };
+    gulp.start('default');
 });
 
-gulp.task('production-3d', ['clean'], function() {
-	uglifyOpts = {
-		compress: {
-			sequences: true,
-			properties: true,
-			dead_code: true,
-			conditionals: true,
-			comparisons: true,
-			evaluate: true,
-			booleans: true,
-			loops: true,
-			unused: true,
-			drop_console: true
-		}
-	};
-	prepOpts = {
-		context: {
-			NODE_ENV: 'production',
-			DEBUG: false,
-			type3D: true
-		}
-	};
-	gulp.start('default');
+gulp.task('production-3d', ['clean'], function () {
+    uglifyOpts = {
+        compress: {
+            sequences: true,
+            properties: true,
+            dead_code: true,
+            conditionals: true,
+            comparisons: true,
+            evaluate: true,
+            booleans: true,
+            loops: true,
+            unused: true,
+            drop_console: true
+        }
+    };
+    prepOpts = {
+        context: {
+            NODE_ENV: 'production',
+            DEBUG: false,
+            type3D: true
+        }
+    };
+    gulp.start('default');
 });
 
-gulp.task('2d', function() {
-	prepOpts = {
-		context: {
-			NODE_ENV: 'production',
-			DEBUG: false,
-			type2D: true
-		}
-	};
-	gulp.start('default');
+gulp.task('2d', function () {
+    prepOpts = {
+        context: {
+            NODE_ENV: 'production',
+            DEBUG: false,
+            type2D: true
+        }
+    };
+    gulp.start('default');
 });
 
-gulp.task('3d', function() {
-	prepOpts = {
-		context: {
-			NODE_ENV: 'production',
-			DEBUG: false,
-			type3D: true
-		}
-	};
-	gulp.start('default');
+gulp.task('3d', function () {
+    prepOpts = {
+        context: {
+            NODE_ENV: 'production',
+            DEBUG: false,
+            type3D: true
+        }
+    };
+    gulp.start('default');
 });
 
-gulp.task('browserSync', function() {
-	browserSync.use(browserSync.spa({
-		selector: "[ng-app]",
-		history: {
-			index: '/index.html'
-		}
-	}));
-	browserSync.init({
-		port: 8080,
-		server: {
-			baseDir: baseDir,
-			files: baseDir + "/*"
-			/*routes: {
-			 "node_modules": "node_modules"
-			 }*/
-		},
-		ui: {
-			port: 8081
-		},
-		logLevel: "info"
-		/* https: true */
-	})
+gulp.task('browserSync', function () {
+    browserSync.use(browserSync.spa({
+        selector: "[ng-app]",
+        history: {
+            index: '/index.html'
+        }
+    }));
+    browserSync.init({
+        port: 8080,
+        server: {
+            baseDir: baseDir,
+            files: baseDir + "/*"
+            /*routes: {
+             "node_modules": "node_modules"
+             }*/
+        },
+        ui: {
+            port: 8081
+        },
+        logLevel: "info"
+        /* https: true */
+    })
 });
 
-gulp.task('clean', function() {
-	return del([distFolder + '**', '!' + distFolder], {
-		force: true
-	});
+gulp.task('clean', function () {
+    return del([distFolder + '**', '!' + distFolder], {
+        force: true
+    });
 });
 
-gulp.task('html', function() {
-	pump([
-		gulp.src('./src/*.html'),
-		gulp.prep(prepOpts),
-		gulp.dest(distFolder + ''),
-		browserSync.reload({
-			stream: true
-		})
-	]);
+gulp.task('html', function () {
+    pump([
+        gulp.src('./src/*.html'),
+        gulp.prep(prepOpts),
+        gulp.dest(distFolder + ''),
+        browserSync.reload({
+            stream: true
+        })
+    ]);
 });
 
-gulp.task('views', function() {
-	return gulp.src(['./src/views/*.html'])
-		.pipe(gulp.dest(distFolder + 'views/'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('views', function () {
+    return gulp.src(['./src/views/*.html'])
+        .pipe(gulp.dest(distFolder + 'views/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('vendor', function() {
-	var files = JSON.parse(fs.readFileSync('vendor.json'));
-	pump([
-		gulp.src(files),
-		gulp.expect(files),
-		gulp.prep(),
-		gulp.babel(),
-		gulp.uglify({
-			mangle: true
-		}),
-		gulp.concat('vendor.js'),
-		gulp.dest(distFolder + 'lib/js'),
-		browserSync.reload({
-			stream: true
-		})
-	], function(err) {
-		if (!!err) console.log("VENDOR::err:", err);
-	});
+gulp.task('vendor', function () {
+    var files = JSON.parse(fs.readFileSync('vendor.json'));
+    pump([
+        gulp.src(files),
+        gulp.expect(files),
+        gulp.prep(),
+        gulp.babel(),
+        gulp.uglify({
+            mangle: true
+        }),
+        gulp.concat('vendor.js'),
+        gulp.dest(distFolder + 'lib/js'),
+        browserSync.reload({
+            stream: true
+        })
+    ], function (err) {
+        if (!!err) console.log("VENDOR::err:", err);
+    });
 });
 
-gulp.task('font', function() {
-	return gulp.src([
-			'./src/fonts/**/*.*',
-			'./node_modules/font-awesome/fonts/*'
-		])
-		.pipe(gulp.dest(distFolder + 'lib/fonts'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('font', function () {
+    return gulp.src([
+        './src/fonts/**/*.*',
+        './node_modules/font-awesome/fonts/*'
+    ])
+        .pipe(gulp.dest(distFolder + 'lib/fonts'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('jshint', function() {
-	return gulp.src('src/js/**/*.js')
-		.pipe(gulp.jshint())
-		.pipe(gulp.jshint.reporter('default'));
+gulp.task('jshint', function () {
+    return gulp.src('src/js/**/*.js')
+        .pipe(gulp.jshint())
+        .pipe(gulp.jshint.reporter('default'));
 })
 
-gulp.task('controllers', [''], function() {
-	pump([
-		gulp.src([
-			'./src/index.js',
-			'./src/js/controllers/**/*.js'
-		]),
-		gulp.prep(prepOpts),
-		gulp.babel(),
-		gulp.dest(distFolder + 'lib/js/'),
-		browserSync.reload({
-			stream: true
-		})
-	], function(err) {
-		if (!!err) console.log("CONTROLLERS::err:", err);
-	})
+gulp.task('controllers', [''], function () {
+    pump([
+        gulp.src([
+            './src/index.js',
+            './src/js/controllers/**/*.js'
+        ]),
+        gulp.prep(prepOpts),
+        gulp.babel(),
+        gulp.dest(distFolder + 'lib/js/'),
+        browserSync.reload({
+            stream: true
+        })
+    ], function (err) {
+        if (!!err) console.log("CONTROLLERS::err:", err);
+    })
 });
 
-gulp.task('js', function() {
-	return gulp.src([
-			'node_modules/angular-loading-bar/build/loading-bar.js'
-		])
-		.pipe(gulp.dest(distFolder + 'lib/js/'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('js', function () {
+    return gulp.src([
+        'node_modules/angular-loading-bar/build/loading-bar.js'
+    ])
+        .pipe(gulp.dest(distFolder + 'lib/js/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('minifyJS', function() {
-	console.log("minifyJS.prepOpts:", prepOpts);
-	console.log("minifyJS.uglifyOpts:", uglifyOpts);
-	pump([
-		gulp.src([
-			'./src/js/app.js',
-			'./src/js/controllers/*Controller.js',
-			'./src/js/services/*Service.js',
-			'./src/js/directives/*.js'
-		]),
-		gulp.concat('main.js'),
-		gulp.prep(prepOpts),
-		gulp.babel(),
-		gulp.uglify(uglifyOpts),
-		gulp.dest(distFolder + '/lib/js'),
-		browserSync.reload({
-			stream: true
-		})
-	], function(err) {
-		if (typeof err != "undefined")
-			console.log("minifiJS.err:", err);
-	});
+gulp.task('minifyJS', function () {
+    console.log("minifyJS.prepOpts:", prepOpts);
+    console.log("minifyJS.uglifyOpts:", uglifyOpts);
+    pump([
+        gulp.src([
+            './src/js/app.js',
+            './src/js/controllers/*Controller.js',
+            './src/js/services/*Service.js',
+            './src/js/directives/*.js'
+        ]),
+        gulp.concat('main.js'),
+        gulp.prep(prepOpts),
+        gulp.babel(),
+        gulp.uglify(uglifyOpts),
+        gulp.dest(distFolder + '/lib/js'),
+        browserSync.reload({
+            stream: true
+        })
+    ], function (err) {
+        if (typeof err != "undefined")
+            console.log("minifiJS.err:", err);
+    });
 });
 
-gulp.task('json', function() {
-	return gulp.src('./src/modules/keyboard/*.json')
-		.pipe(gulp.dest(distFolder + '/'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('json', function () {
+    return gulp.src('./src/modules/keyboard/*.json')
+        .pipe(gulp.dest(distFolder + '/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('img', function() {
-	return gulp.src('./src/img/*')
-		.pipe(gulp.dest(distFolder + 'lib/img'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('img', function () {
+    return gulp.src('./src/img/*')
+        .pipe(gulp.dest(distFolder + 'lib/img'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('css', function() {
-	return gulp.src([
-			'./node_modules/font-awesome/css/font-awesome.min.css',
-			'./node_modules/angular-loading-bar/build/loading-bar.css'
-		])
-		.pipe(gulp.dest(distFolder + 'lib/css'))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
+gulp.task('css', function () {
+    return gulp.src([
+        './node_modules/font-awesome/css/font-awesome.min.css',
+        './node_modules/angular-loading-bar/build/loading-bar.css'
+    ])
+        .pipe(gulp.dest(distFolder + 'lib/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('less', function() {
-	pump([
-		gulp.src('./src/less/styles.less'),
-		gulp.less({
-			paths: [path.join(__dirname, 'less')]
-		}),
-		gulp.dest(distFolder + 'lib/css'),
-		browserSync.reload({
-			stream: true
-		})
-	], function(err) {
-		if (typeof err != "undefined")
-			console.log("LESS::err:", err);
-	});
+gulp.task('less', function () {
+    pump([
+        gulp.src('./src/less/styles.less'),
+        gulp.less({
+            paths: [path.join(__dirname, 'less')]
+        }),
+        gulp.dest(distFolder + 'lib/css'),
+        browserSync.reload({
+            stream: true
+        })
+    ], function (err) {
+        if (typeof err !== "undefined")
+            console.log("LESS::err:", err);
+    });
 
 });
 
-
-gulp.task('layout', function() {
-	return gulp.src('./layout.json')
-		.pipe(gulp.dest(distFolder))
-		.pipe(browserSync.reload({
-			stream: true
-		}));
-});
-
-gulp.task('rewrite', function() {
-	pump([
-		gulp.src('./src/.htaccess'),
-		gulp.dest('./dist/')
-	], function(err) {
-		if (!!err) console.log("rewrite:err:", err);
-	});
-});
-
-gulp.task('watch-bs', ['3d', 'browserSync'], function() {
-	prepOpts = {
-		context: {
-			NODE_ENV: 'development',
-			DEBUG: true,
-			type3D: true
-		}
-	};
-	gulp.watch('src/*.html', ['html']);
-	gulp.watch('src/views/*.html', ['views']);
-	gulp.watch('src/less/*.less', ['less']);
-	gulp.watch('./src/font/*', ['font']);
-	gulp.watch([
-		'vendor.json',
-		JSON.parse(fs.readFileSync('vendor.json'))
-	], ['vendor']);
-	gulp.watch('src/images/png/*', ['img']);
-	gulp.watch('src/js/**/*.js', ['minifyJS']);
-	gulp.watch('src/.htaccess', ['rewrite']);
+gulp.task('dist-less', function () {
+    pump([
+        gulp.src(['./src/less/mixins.less', './src/less/styles.less', './src/less/variables.less', './src/less/variables_private.less',]),
+        gulp.dest(distFolder + 'less'),
+        browserSync.reload({
+            stream:true
+        })
+    ], function (err) {
+        if (typeof err !== "undefined")
+            console.log("DIST-LESS::err:", err);
+    })
 });
 
 
-
-gulp.task('watch-bs2d', ['2d', 'browserSync'], function() {
-	prepOpts = {
-		context: {
-			NODE_ENV: 'development',
-			DEBUG: true,
-			type2D: true
-		}
-	};
-	gulp.watch('src/*.html', ['html']);
-	gulp.watch('src/views/*.html', ['views']);
-	gulp.watch('src/less/*.less', ['less']);
-	gulp.watch('./src/font/*', ['font']);
-	gulp.watch('vendor.json', ['vendor']);
-	gulp.watch('src/images/png/*', ['img']);
-	gulp.watch('src/js/**/*.js', ['minifyJS']);
-	gulp.watch('src/.htaccess', ['rewrite']);
+gulp.task('layout', function () {
+    return gulp.src('./layout.json')
+        .pipe(gulp.dest(distFolder))
+        .pipe(browserSync.reload({
+            stream: true
+        }));
 });
 
-gulp.task('watch-hs', ['3d'], function() {
-	gulp.watch('src/*.html', ['html']);
-	gulp.watch('src/views/*.html', ['views']);
-	gulp.watch('src/less/*.less', ['less']);
-	gulp.watch('./src/font/*', ['font']);
-	gulp.watch('vendor.json', ['vendor']);
-	gulp.watch('src/images/png/*', ['img']);
-	gulp.watch('src/js/**/*.js', ['minifyJS']);
-	gulp.watch('src/.htaccess', ['rewrite']);
+gulp.task('rewrite', function () {
+    pump([
+        gulp.src('./src/.htaccess'),
+        gulp.dest('./dist/')
+    ], function (err) {
+        if (!!err) console.log("rewrite:err:", err);
+    });
+});
+
+gulp.task('watch-bs', ['3d', 'browserSync'], function () {
+    prepOpts = {
+        context: {
+            NODE_ENV: 'development',
+            DEBUG: true,
+            type3D: true
+        }
+    };
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/views/*.html', ['views']);
+    gulp.watch('src/less/*.less', ['less','dist-less']);
+    gulp.watch('./src/font/*', ['font']);
+    gulp.watch([
+        'vendor.json',
+        JSON.parse(fs.readFileSync('vendor.json'))
+    ], ['vendor']);
+    gulp.watch('src/images/png/*', ['img']);
+    gulp.watch('src/js/**/*.js', ['minifyJS']);
+    gulp.watch('src/.htaccess', ['rewrite']);
+});
+
+
+gulp.task('watch-bs2d', ['2d', 'browserSync'], function () {
+    prepOpts = {
+        context: {
+            NODE_ENV: 'development',
+            DEBUG: true,
+            type2D: true
+        }
+    };
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/views/*.html', ['views']);
+    gulp.watch('src/less/*.less', ['less','dist-less']);
+    gulp.watch('./src/font/*', ['font']);
+    gulp.watch('vendor.json', ['vendor']);
+    gulp.watch('src/images/png/*', ['img']);
+    gulp.watch('src/js/**/*.js', ['minifyJS']);
+    gulp.watch('src/.htaccess', ['rewrite']);
+});
+
+gulp.task('watch-hs', ['3d'], function () {
+    gulp.watch('src/*.html', ['html']);
+    gulp.watch('src/views/*.html', ['views']);
+    gulp.watch('src/less/*.less', ['less','dist-less']);
+    gulp.watch('./src/font/*', ['font']);
+    gulp.watch('vendor.json', ['vendor']);
+    gulp.watch('src/images/png/*', ['img']);
+    gulp.watch('src/js/**/*.js', ['minifyJS']);
+    gulp.watch('src/.htaccess', ['rewrite']);
 });
